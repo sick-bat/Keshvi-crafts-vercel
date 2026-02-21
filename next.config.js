68e01d8c8d346099f63f1c0ad8a3d7dd37a426e4
sachin Singh <sickbat7@gmail.com>
2026-02-21 18:38:34 +0530

    www issue broken link

--- next.config.js
+++ next.config.js
@@ -4,21 +4,6 @@ const isExport = process.env.NEXT_EXPORT === 'true';
 const nextConfig = {
   // Only export for production builds when we explicitly ask for it
   ...(isExport ? { output: 'export' } : {}),
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.keshvicrafts.in',
-          },
-        ],
-        destination: 'https://keshvicrafts.in/:path*',
-        permanent: true,
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
