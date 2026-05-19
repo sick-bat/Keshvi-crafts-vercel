bfd78ff846575e31c8667ec831fc9c9d4334a2c7
sachin Singh <sickbat7@gmail.com>
2026-05-19 23:09:32 +0530

    Payment Gateway

--- src/app/api/payu/response/route.ts
+++ src/app/api/payu/response/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import crypto from 'crypto';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
